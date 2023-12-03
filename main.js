b2158d1ba4e93889417ea4e105165ad48a1463e6
llopez <lueunbii98@gmail.com>
2023-12-03 14:19:46 -0500

    feat: configuración inicial y montaje de modelo 3d

---
+++ main.js
@@ -0,0 +1,120 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+let camera, scene, renderer;
+
+init();
+render();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+  );
+  camera.position.set(1.5, 4, 9);
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xf6eedc);
+  /*   const light = new THREE.AmbientLight(0xffffff); // soft white light
+  scene.add(light); */
+
+  //
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
+  hemiLight.color.setHSL(0.6, 1, 0.6);
+  hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+  hemiLight.position.set(0, 50, 0);
+  scene.add(hemiLight);
+
+  const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+  scene.add(hemiLightHelper);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+  dirLight.color.setHSL(0.1, 1, 0.95);
+  dirLight.position.set(-1, 1.75, 1);
+  dirLight.position.multiplyScalar(30);
+  scene.add(dirLight);
+
+  dirLight.castShadow = true;
+
+  dirLight.shadow.mapSize.width = 2048;
+  dirLight.shadow.mapSize.height = 2048;
+
+  const d = 50;
+
+  dirLight.shadow.camera.left = -d;
+  dirLight.shadow.camera.right = d;
+  dirLight.shadow.camera.top = d;
+  dirLight.shadow.camera.bottom = -d;
+
+  dirLight.shadow.camera.far = 3500;
+  dirLight.shadow.bias = -0.0001;
+
+  /*   const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 10);
+  scene.add(dirLightHelper); */
+
+  const groundGeo = new THREE.PlaneGeometry(10000, 10000);
+  const groundMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  groundMat.color.setHSL(0.095, 1, 0.75);
+
+  const ground = new THREE.Mesh(groundGeo, groundMat);
+  ground.position.y = -33;
+  ground.rotation.x = -Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  //
+
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath('jsm/libs/draco/gltf/');
+
+  const loader = new GLTFLoader();
+  loader.setDRACOLoader(dracoLoader);
+  loader.load('island.gltf', function (gltf) {
+    gltf.scene.traverse(function (node) {
+      if (node.isMesh) {
+        node.castShadow = true;
+        node.receiveShadow = true;
+      }
+    });
+    scene.add(gltf.scene);
+
+    render();
+  });
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+
+  document.body.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render);
+  controls.target.set(0, 2, 0);
+  controls.update();
+
+  window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
+  renderer.render(scene, camera);
+}
